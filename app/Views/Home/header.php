eddbf7ef9c65e1af58aee7cd45e35d3adc65ac56
Vitor <vitoralannl@gmail.com>
2021-03-21 18:19:19 -0300

    Pequena alteração na view home/comentários

--- app/Views/Home/header.php
+++ app/Views/Home/header.php
@@ -6,12 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/header.css'); ?>">
-    
     <title>Projeto PBL</title>
 </head>
 <body class="container">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid fs-6">
+  <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
