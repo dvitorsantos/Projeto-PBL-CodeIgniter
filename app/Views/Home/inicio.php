4e46233f2a8697f11821dabbc9851ff55c388efe
pedro-alucard <toxical007@gmail.com>
2021-03-21 17:46:21 -0300

    ta

--- app/Views/Home/inicio.php
+++ app/Views/Home/inicio.php
@@ -5,18 +5,94 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/inicio.css'); ?>">
     <title>Incio</title>
 </head>
 <body>
-<div class="container">
-  <div class="row row-cols-3">
-    <div class="col">Salve</div>
-    <div class="col">Oi</div>
-    <div class="col">saca</div>
+
+
+
+    <!-- comeco container -->
+<div class="container-sm">
+
+<div id="slide">
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?php echo base_url('assets/images/natureza1.jpeg');?>" class="d-block w-100" alt="natureza1">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo base_url('assets/images/natureza1.jpeg');?>" class="d-block w-100" alt="natureza2">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo base_url('assets/images/natureza1.jpeg');?>" class="d-block w-100" alt="natureza3">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"  data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
 
+</div>
+
+
+
+
+
+
+
+
+
+
+ <!-- comeco conteudo -->
+<div id="conteudo">
+
+
+  <div class="row">
+    <div class="col-8">
+        
+    <div class="">
+    <h1 class="display-6 text-center">Text 1</h1>
+    </div>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis auctor elit sed vulputate mi sit amet. Cursus in hac habitasse platea dictumst quisque. Cursus vitae congue mauris rhoncus. Leo integer malesuada nunc vel risus commodo viverra. Tortor at risus viverra adipiscing at. Sed egestas egestas fringilla phasellus faucibus. Pulvinar pellentesque habitant morbi tristique senectus et. Scelerisque eu ultrices vitae auctor eu augue ut lectus arcu. Adipiscing elit duis tristique sollicitudin nibh sit amet.</p>
+
+    </div>
+
+    <div class="col">
+      2 of 3 (wider)
+    </div>
+  </div>
+
+</div>
+<!-- fim conteudo -->
+
+</div>
+<!-- fim do conteiner -->
 
 </body>
 </html>
