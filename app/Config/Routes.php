922f5f6b80f3ec9c72057ce0c0f0eb121e64a3b2
Vitor <vitoralannl@gmail.com>
2021-03-21 16:12:18 -0300

    Alteração da página de comentários

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,7 +33,6 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Landing::index');
-$routes->add('home/comentarios', 'Home::comentarios');
 
 /*
  * --------------------------------------------------------------------
--- app/Views/Home/comentarios.php
+++ app/Views/Home/comentarios.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/comentarios.css'); ?>">
     <title>Document</title>
 </head>
 <body>
+    <div class="title"><h1>Comentários</h1></div>
 <?php
     for ($i = 0; $i < count($comentarios); $i = $i + 2) {
         echo "<div class='card'>";
@@ -18,6 +19,8 @@
         echo "</div>";
     }
 ?>
-<a class="btn btn-success" href="adicionar_comentario" role="button">Adicionar Comentário</a>
+<div class="add-comentario">
+    <a class="btn btn-success" href="adicionar_comentario" role="button">Adicionar Comentário</a>
+</div>
 </body>
 </html>
